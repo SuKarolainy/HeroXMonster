1d12bc85f15e2f17ddeb4c51cf0ae5009a82452c
Suellen Karolainy <103966382+SuKarolainy@users.noreply.github.com>
2024-03-21 10:04:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} and now has {monster} health.");
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} and now has {hero} health.");
+} while (hero > 0 && monster > 0);
+Console.WriteLine(hero > monster ? "Hero wins!" : " Monster wins!");
+        
